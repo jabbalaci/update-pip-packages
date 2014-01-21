a1ab5cca9a91350db27cc8a40d09a303ace085da
Jabba Laci <jabba.laci@gmail.com>
2014-01-21 16:20:52 +0100

    Py3 compatibility

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 update-pip-packages
 ===================
 
-update all the packages (in alphabetical order) that were installed with pip
+Update all the packages (in alphabetical order)
+that you have installed globally with pip
+(i.e. with `sudo pip install`).
+
+<http://pythonadventures.wordpress.com/2013/05/22/update-all-pip-packages/>
+
+Jabba Laci, 2013--2014 (jabba.laci@gmail.com)
--- update-pip.py
+++ update-pip.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python
 
+"""
+Update all the packages (in alphabetical order)
+that you have installed globally with pip
+(i.e. with `sudo pip install`).
+
+http://pythonadventures.wordpress.com/2013/05/22/update-all-pip-packages/
+
+Jabba Laci, 2013--2014 (jabba.laci@gmail.com)
+"""
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
 import os
+
 import pip
+
 
 dists = []
 for dist in pip.get_installed_distributions():
@@ -12,5 +27,5 @@ dists.insert(0, 'pip')  # let 'pip' be the first
 
 for dist_name in dists:
     cmd = "sudo pip install {0} -U".format(dist_name)
-    print '#', cmd
+    print('#', cmd)
     os.system(cmd)
